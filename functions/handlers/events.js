ef810a7752288c25c86591adf6adaeb6f255d9fc
Dan Lenington <danlening02@gmail.com>
2020-03-12 18:59:47 -0500

    Update
    update function to get presenters/details for events

--- functions/handlers/events.js
+++ functions/handlers/events.js
@@ -52,32 +52,32 @@ exports.getEvent = (req, res) => {
     });
 };
 
-// exports.getPanel = (req, res) => {
-//   let panelData = {};
-//   db.doc(`/panel/${req.params.panelId}`)
-//     .get()
-//     .then(doc => {
-//       if (!doc.exists) {
-//         return res.status(404).json({
-//           error: "Event not found"
-//         });
-//       }
-//       eventData = doc.data();
-//       eventData.eventId = doc.id;
-//       return db
-//         .collection("panels")
-//         .where("eventId", "==", req.params.eventId)
-//         .get();
-//     })
-//     .then(data => {
-//       eventData.panels = [];
-//       data.forEach(doc => {
-//         eventData.panels.push(doc.data());
-//       });
-//       return res.json(eventData);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({ error: err.code });
-//     });
-// };
+exports.getDetails = (req, res) => {
+  let panelData = {};
+  db.doc(`/panel/${req.params.panelId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({
+          error: "Event not found"
+        });
+      }
+      eventData = doc.data();
+      eventData.eventId = doc.id;
+      return db
+        .collection("panels")
+        .where("eventId", "==", req.params.eventId)
+        .get();
+    })
+    .then(data => {
+      eventData.panels = [];
+      data.forEach(doc => {
+        eventData.panels.push(doc.data());
+      });
+      return res.json(eventData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
