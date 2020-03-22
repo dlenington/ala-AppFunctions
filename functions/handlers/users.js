816d81c732d5f3a50b75d8af45fa315174f46ccf
Dan Lenington <danlening02@gmail.com>
2020-03-22 10:56:10 -0500

    Login and Signup
    Update login and signup functions.

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -4,7 +4,7 @@ const config = require("../util/config");
 const firebase = require("firebase");
 firebase.initializeApp(config);
 
-const { validateSignupData } = require("../util/validators");
+const { validateSignupData, validateLoginData } = require("../util/validators");
 
 exports.signup = (req, res) => {
   const newUser = {
@@ -52,5 +52,32 @@ exports.signup = (req, res) => {
       return res
         .status(500)
         .json({ general: "Something went wrong please try again" });
+    });
+};
+
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  const { valid, errors } = validateLoginData(user);
+
+  if (!valid) return res.status(400).json({ errors });
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      return res
+        .status(403)
+        .json({ general: "Wrong credentials, please try again" });
     });
 };
