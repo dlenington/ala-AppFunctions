5da3e9c177d3ae270aa39d34864b16e952c3af24
Dan Lenington <danlening02@gmail.com>
2020-03-24 09:53:44 -0500

    Login Route
    Update

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,12 @@ const { db } = require("./util/admin");
 const app = require("express")();
 
 const { getEvents, getEvent, getPanelData } = require("./handlers/events");
-const { signup } = require("./handlers/users");
+const { signup, login } = require("./handlers/users");
 
 app.get("/events/:day", getEvents);
 app.get("/event/:eventId", getEvent);
 app.get("/panel/:panelId", getPanelData);
 app.post("/signup", signup);
+app.post("/login", login);
 
 exports.api = functions.https.onRequest(app);
