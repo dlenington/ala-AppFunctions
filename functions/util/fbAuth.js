df8adc5edf77f793319ae3244746e733decdb7f4
Dan Lenington <danlening02@gmail.com>
2020-04-28 10:18:34 -0500

    Update fbAuth.js

--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -17,5 +17,18 @@ module.exports = (req, res, next) => {
     .verifyIdToken(idToken)
     .then((decodedToken) => {
       req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get(1);
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
     });
 };
